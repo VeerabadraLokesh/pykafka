5f5060280eb28dcd0c728f7c5b7f07ff1ee8db8e
Lokesh Paturu <veerabadralokesh@pitt.edu>
2023-04-21 17:43:29 -0400

    changes

---
+++ file_manager.py
@@ -0,0 +1,84 @@
+
+
+
+import os
+import json
+from glob import glob
+
+import settings as S
+
+class FileManager:
+    def __init__(self, root) -> None:
+        self.root = root
+        self.fast_root = os.path.join(root, "cache")
+        os.makedirs(self.fast_root, exist_ok=True)
+        # for i in S.SERVER_IDS:
+        #     os.makedirs(os.path.join(self.fast_root, str(i)), exist_ok=True)
+
+    
+    def write(self, file, message):
+        if isinstance(message, dict):
+            message = json.dumps(message)
+        path = os.path.join(self.root, file)
+        with open(path, 'w') as f:
+            f.write(message)
+    
+    def fast_write(self, file, message: bytes):
+        try:
+            path = os.path.join(self.fast_root, file)
+            file_desc = os.open(path, os.O_RDWR | os.O_CREAT)
+            os.write(file_desc, message)     
+            os.close(file_desc)
+        except Exception as e:
+            print(f'Error in fast_write: {e}')
+
+    def read(self, file):
+        path = os.path.join(self.root, file)
+        if glob(path):
+            with open(path, 'r') as f:
+                lines = f.read()
+            return lines
+    
+    def fast_read(self, file) -> bytes:
+        try:
+            path = os.path.join(self.fast_root, file)
+            file_size = os.path.getsize(path)
+            file_desc = os.open(path, os.O_RDONLY)
+            lines = os.read(file_desc, file_size)
+            os.close(file_desc)
+            return lines
+        except Exception as e:
+            print(f'Error in fast_read: {e}')
+
+    def readlines(self, file):
+        path = os.path.join(self.root, file)
+        if glob(path):
+            with open(path, 'r') as f:
+                lines = f.readlines()
+            return lines
+    
+    def append(self, file, message):
+        if isinstance(message, dict):
+            message = json.dumps(message)
+        path = os.path.join(self.root, file)
+        with open(path, 'a') as f:
+            f.write(message)
+            f.write('\n')
+    
+    def delete_file(self, file, fast=False):
+        try:
+            if fast:
+                os.remove(os.path.join(self.fast_root, file))
+            else:
+                os.remove(os.path.join(self.root, file))
+        except OSError:
+            pass
+    
+    def list_files(self, path=None, fast=False):
+        if fast:
+            if path:
+                return os.listdir(os.path.join(self.fast_root, path))
+            return os.listdir(self.fast_root)
+        if path:
+            return os.listdir(os.path.join(self.root, path))
+        return os.listdir(self.root)
