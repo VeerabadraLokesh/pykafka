4d3adc8d7b5cc0cb380474c6e6dbbfa1278fd5c2
Lokesh Paturu <veerabadralokesh@pitt.edu>
2023-04-24 10:02:16 -0400

    changes

--- test_consumer.py
+++ test_consumer.py
@@ -1,6 +1,8 @@
 
 import time
 import logging
+import numpy as np
+import matplotlib.pyplot as plt
 
 from KafkaConsumer import KafkaConsumer
 import settings as S
@@ -25,6 +27,8 @@ if __name__ == "__main__":
     total_bytes = 0
     start_time = time.perf_counter()
 
+    message_timestamps = [[0, start_time]]
+
     message_index = 0
     for message in kafka_consumer.createMessageStreams():
         if message != b' ':
@@ -32,6 +36,7 @@ if __name__ == "__main__":
             total_bytes += len(message)
             if message_index % 100 == 99:
                 logging.info(f"Received {(message_index+1)} in {time.perf_counter() - start_time:.3f} seconds")
+                message_timestamps.append([message_index, time.perf_counter()])
             message_index += 1
             if message_index == S.TEST_MESSAGE_COUNT:
                 break
@@ -42,3 +47,18 @@ if __name__ == "__main__":
 
     logging.info(f"Received {S.TEST_MESSAGE_COUNT} in {time_taken:.3f} seconds")
     logging.info(f"{tmb} MiB received in {time_taken:.3f} seconds. Rate: {mibps:.3f} MiB/s")
+    
+    message_timestamps = np.array(message_timestamps)
+
+    message_timestamps[:, 1] -= message_timestamps[0, 1]
+
+    try:
+        plt.figure()
+        plt.plot(message_timestamps[:,0], message_timestamps[:,1])
+        plt.xlabel('Messages delivered')
+        plt.ylabel('seconds')
+        plt.title(f"Reading {S.TEST_MESSAGE_COUNT} - {(S.TEST_MESSAGE_SIZE/1024)} KiB messages - {mibps:.2f} MiBps")
+        plt.savefig(f"{int(time.time()*100_000_000)}.svg")
+        plt.show()
+    except:
+        pass
